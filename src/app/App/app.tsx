a7e5c8cb6c5cd2b53584489d0365d410d0342912
sornmonika <monika.sorn@institute.pse.ngo>
2025-03-11 15:42:03 +0700

    new update

--- src/app/App/app.tsx
+++ src/app/App/app.tsx
@@ -24,7 +24,6 @@ const App = () => {
             {init && (
                 <Particles
                     id="tsparticles"
-                    // particlesLoaded={particlesLoaded}
                     options={{
                         fpsLimit: 120,
                         interactivity: {
